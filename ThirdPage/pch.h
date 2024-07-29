de971617287bf753d6e0d6b22295edc5b770da68
podo3501 <nixie77@naver.com>
2024-07-29 14:36:55 +0900

    DirectXTK12 프로젝트 붙이기

--- ThirdPage/pch.h
+++ ThirdPage/pch.h
@@ -71,8 +71,26 @@
 #include <dxgidebug.h>
 #endif
 
-// If using the DirectX Tool Kit for DX12, uncomment this line:
-//#include "GraphicsMemory.h"
+#include "Audio.h"
+#include "BufferHelpers.h"
+#include "CommonStates.h"
+#include "DirectXHelpers.h"
+#include "DDSTextureLoader.h"
+#include "DescriptorHeap.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Model.h"
+#include "Mouse.h"
+#include "PrimitiveBatch.h"
+#include "ResourceUploadBatch.h"
+#include "RenderTargetState.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h" 
+#include "VertexTypes.h"
 
 namespace DX
 {
