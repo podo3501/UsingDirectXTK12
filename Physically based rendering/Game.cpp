7cfe87ad6527c8598d7de059d01e3c6dd2117315
podo3501 <nixie77@naver.com>
2024-08-03 12:43:52 +0900

    Physically based rendering 완료

--- Physically based rendering/Game.cpp
+++ Physically based rendering/Game.cpp
@@ -20,6 +20,12 @@ Game::Game() noexcept(false)
     //   Add DX::DeviceResources::c_EnableHDR for HDR10 display.
     //   Add DX::DeviceResources::c_ReverseDepth to optimize depth buffer clears for 0 instead of 1.
     m_deviceResources->RegisterDeviceNotify(this);
+
+    m_hdrScene = std::make_unique<DX::RenderTexture>(DXGI_FORMAT_R16G16B16A16_FLOAT);
+
+    XMVECTORF32 color;
+    color.v = XMColorSRGBToRGB(Colors::CornflowerBlue);
+    m_hdrScene->SetClearColor(color);
 }
 
 Game::~Game()
@@ -66,10 +72,9 @@ void Game::Update(DX::StepTimer const& timer)
 {
     PIXBeginEvent(PIX_COLOR_DEFAULT, L"Update");
 
-    float elapsedTime = float(timer.GetElapsedSeconds());
+    auto time = static_cast<float>(timer.GetTotalSeconds());
 
-    // TODO: Add your game logic here.
-    elapsedTime;
+    m_world = Matrix::CreateRotationY(cosf(time) * 2.f);
 
     PIXEndEvent();
 }
@@ -87,23 +92,30 @@ void Game::Render()
 
     // Prepare the command list to render a new frame.
     m_deviceResources->Prepare();
-    Clear();
 
     auto commandList = m_deviceResources->GetCommandList();
-    PIXBeginEvent(commandList, PIX_COLOR_DEFAULT, L"Render");
+    m_hdrScene->BeginScene(commandList);
+
+    Clear();
 
     // TODO: Add your rendering code here.
+    ID3D12DescriptorHeap* heaps[] = { m_resourceDescriptors->Heap(), m_states->Heap() };
+    commandList->SetDescriptorHeaps(static_cast<UINT>(std::size(heaps)), heaps);
 
-    PIXEndEvent(commandList);
+    m_effect->SetWorld(m_world);
+    m_effect->Apply(commandList);
+    m_shape->Draw(commandList);
+
+    m_hdrScene->EndScene(commandList);
+
+    auto rtvDescriptor = m_deviceResources->GetRenderTargetView();
+    commandList->OMSetRenderTargets(1, &rtvDescriptor, FALSE, nullptr);
+
+    m_toneMap->Process(commandList);
 
     // Show the new frame.
-    PIXBeginEvent(PIX_COLOR_DEFAULT, L"Present");
     m_deviceResources->Present();
-
-    // If using the DirectX Tool Kit for DX12, uncomment this line:
-    // m_graphicsMemory->Commit(m_deviceResources->GetCommandQueue());
-
-    PIXEndEvent();
+    m_graphicsMemory->Commit(m_deviceResources->GetCommandQueue());
 }
 
 // Helper method to clear the back buffers.
@@ -113,11 +125,13 @@ void Game::Clear()
     PIXBeginEvent(commandList, PIX_COLOR_DEFAULT, L"Clear");
 
     // Clear the views.
-    auto const rtvDescriptor = m_deviceResources->GetRenderTargetView();
+    //auto const rtvDescriptor = m_deviceResources->GetRenderTargetView();
+    auto rtvDescriptor = m_renderDescriptors->GetCpuHandle(RTDescriptors::HDRScene);
     auto const dsvDescriptor = m_deviceResources->GetDepthStencilView();
 
     commandList->OMSetRenderTargets(1, &rtvDescriptor, FALSE, &dsvDescriptor);
-    commandList->ClearRenderTargetView(rtvDescriptor, Colors::CornflowerBlue, 0, nullptr);
+    //commandList->ClearRenderTargetView(rtvDescriptor, Colors::CornflowerBlue, 0, nullptr);
+    m_hdrScene->Clear(commandList);
     commandList->ClearDepthStencilView(dsvDescriptor, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
 
     // Set the viewport and scissor rect.
@@ -202,23 +216,159 @@ void Game::CreateDeviceDependentResources()
     }
 
     // If using the DirectX Tool Kit for DX12, uncomment this line:
-    // m_graphicsMemory = std::make_unique<GraphicsMemory>(device);
+    m_graphicsMemory = std::make_unique<GraphicsMemory>(device);
 
-    // TODO: Initialize device dependent objects here (independent of window size).
+    m_states = std::make_unique<CommonStates>(device);
+    m_shape = GeometricPrimitive::CreateSphere();
+    
+    m_resourceDescriptors = std::make_unique<DescriptorHeap>(device,
+        Descriptors::Count);
+
+    m_renderDescriptors = std::make_unique<DescriptorHeap>(device,
+        D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+        D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
+        RTDescriptors::RTCount);
+
+    m_hdrScene->SetDevice(device,
+        m_resourceDescriptors->GetCpuHandle(Descriptors::SceneTex),
+        m_renderDescriptors->GetCpuHandle(RTDescriptors::HDRScene));
+
+    RenderTargetState rtState(m_deviceResources->GetBackBufferFormat(), DXGI_FORMAT_UNKNOWN);
+
+    m_toneMap = std::make_unique<ToneMapPostProcess>(device,
+        rtState, ToneMapPostProcess::Reinhard, ToneMapPostProcess::SRGB);
+
+    RenderTargetState hdrState(m_hdrScene->GetFormat(), m_deviceResources->GetDepthBufferFormat());
+
+    {
+        EffectPipelineStateDescription pd(
+            &GeometricPrimitive::VertexType::InputLayout,
+            CommonStates::Opaque,
+            CommonStates::DepthDefault,
+            CommonStates::CullCounterClockwise,
+            hdrState);
+
+        m_effect = std::make_unique<PBREffect>(device, EffectFlags::Texture | EffectFlags::Emissive, pd);
+
+        m_effect->EnableDefaultLighting();
+    }
+
+    ResourceUploadBatch resourceUpload(device);
+
+    resourceUpload.Begin();
+
+    m_shape->LoadStaticBuffers(device, resourceUpload);
+
+    /*DX::ThrowIfFailed(
+        CreateDDSTextureFromFile(device, resourceUpload,
+            L"SunSubMixer_diffuseIBL.dds",
+            m_radiance.ReleaseAndGetAddressOf(), true));*/
+
+    DX::ThrowIfFailed(
+        CreateDDSTextureFromFile(device, resourceUpload,
+            L"SunSubMixer_diffuseIBL.dds",
+            m_radiance.ReleaseAndGetAddressOf()));
+
+    CreateShaderResourceView(device, m_radiance.Get(),
+        m_resourceDescriptors->GetCpuHandle(Descriptors::RadianceIBL));
+
+    DX::ThrowIfFailed(
+        CreateDDSTextureFromFile(device, resourceUpload,
+            L"SunSubMixer_specularIBL.dds",
+            m_irradiance.ReleaseAndGetAddressOf()));
+
+    CreateShaderResourceView(device, m_irradiance.Get(),
+        m_resourceDescriptors->GetCpuHandle(Descriptors::IrradianceIBL), true);
+
+    DX::ThrowIfFailed(
+        CreateWICTextureFromFile(device, resourceUpload,
+            L"Sphere2Mat_baseColor.png",
+            m_albedoMap.ReleaseAndGetAddressOf()));
+
+    CreateShaderResourceView(device, m_albedoMap.Get(),
+        m_resourceDescriptors->GetCpuHandle(Descriptors::AlbedoMap));
+
+    DX::ThrowIfFailed(
+        CreateWICTextureFromFile(device, resourceUpload,
+            L"Sphere2Mat_normal.png",
+            m_normalMap.ReleaseAndGetAddressOf()));
+
+    CreateShaderResourceView(device, m_normalMap.Get(),
+        m_resourceDescriptors->GetCpuHandle(Descriptors::NormalMap));
+
+    DX::ThrowIfFailed(
+        CreateWICTextureFromFile(device, resourceUpload,
+            L"Sphere2Mat_occlusionRoughnessMetallic.png",
+            m_rmaMap.ReleaseAndGetAddressOf()));
+
+    CreateShaderResourceView(device, m_rmaMap.Get(),
+        m_resourceDescriptors->GetCpuHandle(Descriptors::RMAMap));
+
+    DX::ThrowIfFailed(
+        CreateWICTextureFromFile(device, resourceUpload,
+            L"Sphere2Mat_emissive.png",
+            m_emissiveMap.ReleaseAndGetAddressOf()));
+
+    CreateShaderResourceView(device, m_emissiveMap.Get(),
+        m_resourceDescriptors->GetCpuHandle(Descriptors::EmissiveMap));
+
+    auto uploadResourcesFinished = resourceUpload.End(
+        m_deviceResources->GetCommandQueue());
+
+    uploadResourcesFinished.wait();
+
+    auto desc = m_radiance->GetDesc();
+
+    auto radianceTex = m_resourceDescriptors->GetGpuHandle(Descriptors::RadianceIBL);
+    auto irradianceTex = m_resourceDescriptors->GetGpuHandle(Descriptors::IrradianceIBL);
+
+    m_effect->SetIBLTextures(radianceTex, desc.MipLevels, irradianceTex, m_states->AnisotropicClamp());
+    m_effect->SetSurfaceTextures(
+        m_resourceDescriptors->GetGpuHandle(Descriptors::AlbedoMap),
+        m_resourceDescriptors->GetGpuHandle(Descriptors::NormalMap),
+        m_resourceDescriptors->GetGpuHandle(Descriptors::RMAMap),
+        m_states->AnisotropicClamp());
+
+    m_effect->SetEmissiveTexture(m_resourceDescriptors->GetGpuHandle(Descriptors::EmissiveMap));
+
+    m_world = Matrix::Identity;
 }
 
 // Allocate all memory resources that change on a window SizeChanged event.
 void Game::CreateWindowSizeDependentResources()
 {
-    // TODO: Initialize windows-size dependent objects here.
+    auto size = m_deviceResources->GetOutputSize();
+    m_view = Matrix::CreateLookAt(Vector3(2.f, 2.f, 2.f),
+        Vector3::Zero, Vector3::UnitY);
+    m_proj = Matrix::CreatePerspectiveFieldOfView(XM_PI / 4.f,
+        float(size.right) / float(size.bottom), 0.1f, 10.f);
+
+    m_effect->SetView(m_view);
+    m_effect->SetProjection(m_proj);
+
+    m_hdrScene->SetWindow(size);
+    auto sceneTex = m_resourceDescriptors->GetGpuHandle(Descriptors::SceneTex);
+    m_toneMap->SetHDRSourceTexture(sceneTex);
 }
 
 void Game::OnDeviceLost()
 {
-    // TODO: Add Direct3D resource cleanup here.
+    m_states.reset();
+    m_shape.reset();
+    m_effect.reset();
+    m_radiance.Reset();
+    m_irradiance.Reset();
+    m_hdrScene->ReleaseDevice();
+    m_toneMap.reset();
+    m_resourceDescriptors.reset();
+    m_renderDescriptors.reset();
+    m_albedoMap.Reset();
+    m_normalMap.Reset();
+    m_rmaMap.Reset();
+    m_emissiveMap.Reset();
 
     // If using the DirectX Tool Kit for DX12, uncomment this line:
-    // m_graphicsMemory.reset();
+    m_graphicsMemory.reset();
 }
 
 void Game::OnDeviceRestored()
--- Physically based rendering/Game.h
+++ Physically based rendering/Game.h
@@ -6,7 +6,7 @@
 
 #include "DeviceResources.h"
 #include "StepTimer.h"
-
+#include "RenderTexture.h"
 
 // A basic game implementation that creates a D3D12 device and
 // provides a game loop.
@@ -62,5 +62,45 @@ private:
     DX::StepTimer                               m_timer;
 
     // If using the DirectX Tool Kit for DX12, uncomment this line:
-    // std::unique_ptr<DirectX::GraphicsMemory> m_graphicsMemory;
+    std::unique_ptr<DirectX::GraphicsMemory> m_graphicsMemory;
+
+    DirectX::SimpleMath::Matrix m_world;
+    DirectX::SimpleMath::Matrix m_view;
+    DirectX::SimpleMath::Matrix m_proj;
+
+    std::unique_ptr<DirectX::CommonStates> m_states;
+    std::unique_ptr<DirectX::GeometricPrimitive> m_shape;
+    std::unique_ptr<DirectX::PBREffect> m_effect;
+
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_radiance;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_irradiance;
+
+    std::unique_ptr<DX::RenderTexture> m_hdrScene;
+    std::unique_ptr<DirectX::ToneMapPostProcess> m_toneMap;
+
+    std::unique_ptr<DirectX::DescriptorHeap> m_resourceDescriptors;
+    std::unique_ptr<DirectX::DescriptorHeap> m_renderDescriptors;
+
+    enum Descriptors
+    {
+        SceneTex,
+        RadianceIBL,
+        IrradianceIBL,
+        AlbedoMap,
+        NormalMap,
+        RMAMap,
+        EmissiveMap,
+        Count
+    };
+
+    enum RTDescriptors
+    {
+        HDRScene,
+        RTCount
+    };
+
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_albedoMap;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_normalMap;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_rmaMap;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_emissiveMap;
 };
